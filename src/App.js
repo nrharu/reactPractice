dbba8bf90554dc6c48141bb92fae5d4ce5b3aad8
nrharu <kuuasr8943@gmail.com>
2021-07-12 12:02:08 +0900

    scssimport途中

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import SidemenuLeft from "./component/sidemenu_left_.js";
 import Main from "./component/main.js";
 import SidemenuRight from "./component/sidemenu_right.js";
 import Footer from "./component/footer.js";
+import "./style/style.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className="body">
       <Main />
       <SidemenuLeft />
       <SidemenuRight />
