9ef5f9888b0f1216cdd97e00df44b4f4a26dbbfd
nrharu <kuuasr8943@gmail.com>
2021-07-26 17:23:04 +0900

    sidemenu_left_navigation繰り返し処理実装

--- src/component/sidemenu_left_child/sidemenu_left_grand_child/sidemenu_left_navigation_array.js
+++ src/component/sidemenu_left_child/sidemenu_left_grand_child/sidemenu_left_navigation_array.js
@@ -4,8 +4,6 @@ const SidemenuLeftNavigationArray = (props) => {
       <a href="#top" className="sidemenu_left_navigation_list">
         <div>{props.icon}</div>
         <p className="sidemenu_left_navigation_list_name">{props.name}</p>
-        {/* <div>{sidemenu_left_navigation_icon()}</div>
-        <p>{sidemenu_left_navigation_name()}</p> */}
       </a>
     </li>
   );
--- src/component/sidemenu_left_child/sidemenu_left_navigation_list.js
+++ src/component/sidemenu_left_child/sidemenu_left_navigation_list.js
@@ -10,49 +10,20 @@ import NavigationListIcon7 from "./sidemenu_left_grand_child/navigation_list_ico
 import NavigationListIcon8 from "./sidemenu_left_grand_child/navigation_list_icon_ditails_button";
 
 const SidemenuLeftNavigationList = () => {
-  const sidemenu_left_navigation_list_name = [
-    "Home",
-    "Explore",
-    "Notification",
-    "Message",
-    "Bookmark",
-    "lists",
-    "Profile",
-    "More",
+  const sidemenu_left_navigation_list = [
+    ["Home", <NavigationListIcon1 />],
+    ["Explora", <NavigationListIcon2 />],
+    ["Notification", <NavigationListIcon3 />],
+    ["Message", <NavigationListIcon4 />],
+    ["Bookmark", <NavigationListIcon5 />],
+    ["Lists", <NavigationListIcon6 />],
+    ["Profile", <NavigationListIcon7 />],
+    ["More", <NavigationListIcon8 />],
   ];
-  const sidemenu_left_navigation_list_icon = [
-    <NavigationListIcon1 />,
-    <NavigationListIcon2 />,
-    <NavigationListIcon3 />,
-    <NavigationListIcon4 />,
-    <NavigationListIcon5 />,
-    <NavigationListIcon6 />,
-    <NavigationListIcon7 />,
-    <NavigationListIcon8 />,
-  ];
-  // let test_array = [1, 2, 3, 4, 5, 6, 7];
-  // let test = test_array.map((value)=>{
-  //   return value * 2;
-  // });
-  // console.log(test);
-  const sidemenu_name_icon = () => {
-    // for (let i = 0; i < 9; i++) {
-    //   <SidemenuLeftNavigationArray
-    //     name={sidemenu_left_navigation_list_name[i]}
-    //     icon={sidemenu_left_navigation_list_icon[i]}
-    //   />;
-    // }
-    let list_name = sidemenu_left_navigation_list_name.map((value) => {
-      return;
-    });
-  };
-  console.log(sidemenu_name_icon());
-
-  return (
-    <ul className="sidemenu_left_navigation_list_wrap">
-      {sidemenu_name_icon()}
-    </ul>
-  );
+  const list_name = sidemenu_left_navigation_list.map((name) => {
+    return <SidemenuLeftNavigationArray name={name} />;
+  });
+  return <ul className="sidemenu_left_navigation_list_wrap">{list_name}</ul>;
 };
 
 export default SidemenuLeftNavigationList;
