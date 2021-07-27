afe081e90d5b3a97823443772325aaa83aa88bfe
nrharu <kuuasr8943@gmail.com>
2021-07-27 10:17:05 +0900

    sidemenu_left_navigstionの繰り返し処理の変更完了

--- src/component/sidemenu_left_child/sidemenu_left_grand_child/sidemenu_left_navigation_array.js
+++ src/component/sidemenu_left_child/sidemenu_left_grand_child/sidemenu_left_navigation_array.js
@@ -1,12 +1,5 @@
 const SidemenuLeftNavigationArray = (props) => {
-  return (
-    <div className="_item">
-      {/* <a href="#top" className="sidemenu_left_navigation_list"> */}
-      <div>{props.icon}</div>
-      <p className="sidemenu_left_navigation_list_name_icon">{props.name}</p>
-      {/* </a> */}
-    </div>
-  );
+  return <div className="sidemenu_left_navigation_list_item">{props.name}</div>;
 };
 
 export default SidemenuLeftNavigationArray;
--- src/component/sidemenu_left_child/sidemenu_left_navigation_list.js
+++ src/component/sidemenu_left_child/sidemenu_left_navigation_list.js
@@ -11,39 +11,58 @@ import NavigationListIcon8 from "./sidemenu_left_grand_child/navigation_list_ico
 
 const SidemenuLeftNavigationList = () => {
   const sidemenu_left_navigation_list = [
-    [<NavigationListIcon1 />, "Home"],
-    [<NavigationListIcon2 />, "Explora"],
-    [<NavigationListIcon3 />, "Notification"],
-    [<NavigationListIcon4 />, "Message"],
-    [<NavigationListIcon5 />, "Bookmark"],
-    [<NavigationListIcon6 />, "Lists"],
-    [<NavigationListIcon7 />, "Profile"],
-    [<NavigationListIcon8 />, "More"],
+    [
+      <div>
+        <NavigationListIcon1 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Home</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon2 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Explora</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon3 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Notification</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon4 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Message</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon5 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Bookmark</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon6 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Lists</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon7 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">Profile</p>,
+    ],
+    [
+      <div>
+        <NavigationListIcon8 />
+      </div>,
+      <p className="sidemenu_left_navigation_list_name_icon">More</p>,
+    ],
   ];
   const list_name = sidemenu_left_navigation_list.map((name) => {
     return <SidemenuLeftNavigationArray name={name} />;
   });
-
-  const sidemenu_left_navigation_list_name = [
-    "Home",
-    "Explora",
-    "Notification",
-    "Message",
-    "Bookmark",
-    "Lists",
-    "Profile",
-    "More",
-  ];
-  const sidemenu_left_navigation_list_icon = [
-    <NavigationListIcon1 />,
-    <NavigationListIcon2 />,
-    <NavigationListIcon4 />,
-    <NavigationListIcon5 />,
-    <NavigationListIcon6 />,
-    <NavigationListIcon7 />,
-    <NavigationListIcon8 />,
-  ];
-
   return <li className="sidemenu_left_navigation_list">{list_name}</li>;
 };
 
