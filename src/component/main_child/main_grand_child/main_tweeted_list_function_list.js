4424e330e92b22906423557a5b1d780586ff80c3
nrharu <kuuasr8943@gmail.com>
2021-07-28 10:18:32 +0900

    main_function_listのカウント機能実装

--- src/component/main_child/main_grand_child/main_tweeted_list_function_list.js
+++ src/component/main_child/main_grand_child/main_tweeted_list_function_list.js
@@ -2,11 +2,15 @@ import MainTweetedListCommentIcon from "../main_grand_child_icon/main_tweeted_li
 import MainTweetedListGoodIcon from "../main_grand_child_icon/main_tweeted_list_good_icon.js";
 import MainTweetedListShareIcon from "../main_grand_child_icon/main_tweeted_list_share_icon.js";
 import MainTweetedListRetweetIcon from "../main_grand_child_icon/main_tweeted_list_retweet_icon.js";
+import React, { useState } from "react";
 
 const MainTweetedListFunctionList = () => {
+  const [count_retweet, set_count_retweet] = useState(0);
+  const [count_good, set_count_good] = useState(0);
+
   return (
-    <ul className="main_tweeted_list_function_list">
-      <li className="main_tweeted_list_function">
+    <div className="main_tweeted_list_function_list">
+      <button className="main_tweeted_list_function">
         {/* コメントアイコン */}
         <div className="main_tweeted_list_icon_wrap main_tweeted_list_icon_wrap_comment">
           <MainTweetedListCommentIcon />
@@ -15,8 +19,11 @@ const MainTweetedListFunctionList = () => {
           className="main_tweeted_list_function_count 
             main_tweeted_list_function_count_comment"
         ></p>
-      </li>
-      <li className="main_tweeted_list_function">
+      </button>
+      <button
+        onClick={() => set_count_retweet(count_retweet + 1)}
+        className="main_tweeted_list_function"
+      >
         {/* リツイートアイコン*/}
         <div className="main_tweeted_list_icon_wrap main_tweeted_list_icon_wrap_retweet">
           <MainTweetedListRetweetIcon />
@@ -25,10 +32,14 @@ const MainTweetedListFunctionList = () => {
           className="main_tweeted_list_function_count 
             main_tweeted_list_function_count_retweet"
         >
-          {/* カウント数 */}x
+          {count_retweet}
+          {/* カウント数 */}
         </p>
-      </li>
-      <li className="main_tweeted_list_function">
+      </button>
+      <button
+        onClick={() => set_count_good(count_good + 1)}
+        className="main_tweeted_list_function"
+      >
         {/* いいねアイコン */}
         <div className="main_tweeted_list_icon_wrap main_tweeted_list_icon_wrap_good">
           <MainTweetedListGoodIcon />
@@ -37,10 +48,11 @@ const MainTweetedListFunctionList = () => {
           className="main_tweeted_list_function_count 
             main_tweeted_list_function_count_good"
         >
-          {/* カウント数 */}x
+          {count_good}
+          {/* カウント数 */}
         </p>
-      </li>
-      <li className="main_tweeted_list_function">
+      </button>
+      <button className="main_tweeted_list_function">
         {/* 共有アイコン */}
         <div className="main_tweeted_list_icon_wrap main_tweeted_list_icon_wrap_share">
           <MainTweetedListShareIcon />
@@ -49,8 +61,8 @@ const MainTweetedListFunctionList = () => {
           className="main_tweeted_list_function_count 
             main_tweeted_list_function_count_share"
         ></p>
-      </li>
-    </ul>
+      </button>
+    </div>
   );
 };
 
