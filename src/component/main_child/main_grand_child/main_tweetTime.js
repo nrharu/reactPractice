9777c78590a935abe71794315ee279a2319f14d5
nrharu <kuuasr8943@gmail.com>
2021-08-03 16:16:01 +0900

    main_tweetの送信時の改行適応

--- src/component/main_child/main_grand_child/main_tweetTime.js
+++ src/component/main_child/main_grand_child/main_tweetTime.js
@@ -1,33 +1,33 @@
 import React from "react";
 import MainTweetIconDitails from "../main_grand_child_icon/main_tweet_icon_ditails.js";
-import firebase, { db } from "../../../firestore.js";
+// import firebase, { db } from "../../../firestore.js";
 
 const MainTweetTime = (props) => {
   // const tweet_time = "usagi";
   // console.log(tweet_time);
   //データベース
-  db.collection("users")
-    .doc("875irvF9ySQFlgkggKx9")
-    .get()
-    //
-    .return(
-      <div className="main_tweeted_list_article_header">
-        <div className="main_tweeted_list_article_header_name_wrap">
-          <p className="main_tweeted_list_article_header_account_name">
-            {props.name}
-          </p>
-          <p className="main_tweeted_list_article_header_ID_time">
-            @{props.ID}・{props.time}
-          </p>
-        </div>
-        <div className="main_tweeted_list_article_header_button_wrap">
-          <div className="main_tweetd_list_article_header_button">
-            {/* 詳細ボタン */}
-            <MainTweetIconDitails />
-          </div>
+  // db.collection("users")
+  //   .doc("875irvF9ySQFlgkggKx9")
+  //   .get()
+  //
+  return (
+    <div className="main_tweeted_list_article_header">
+      <div className="main_tweeted_list_article_header_name_wrap">
+        <p className="main_tweeted_list_article_header_account_name">
+          {props.name}
+        </p>
+        <p className="main_tweeted_list_article_header_ID_time">
+          @{props.ID}・{props.time}
+        </p>
+      </div>
+      <div className="main_tweeted_list_article_header_button_wrap">
+        <div className="main_tweetd_list_article_header_button">
+          {/* 詳細ボタン */}
+          <MainTweetIconDitails />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default MainTweetTime;
--- src/component/main_child/main_grand_child/main_tweet_text_area.js
+++ src/component/main_child/main_grand_child/main_tweet_text_area.js
@@ -1,4 +1,64 @@
 const MainTweetTextArea = (props) => {
+  // var calculateContentHeight = function (ta, scanAmount) {
+  //   var origHeight = ta.style.height,
+  //     height = ta.offsetHeight,
+  //     scrollHeight = ta.scrollHeight,
+  //     overflow = ta.style.overflow;
+  //   /// only bother if the ta is bigger than content
+  //   if (height >= scrollHeight) {
+  //     /// check that our browser supports changing dimension
+  //     /// calculations mid-way through a function call...
+  //     ta.style.height = height + scanAmount + "px";
+  //     /// because the scrollbar can cause calculation problems
+  //     ta.style.overflow = "hidden";
+  //     /// by checking that scrollHeight has updated
+  //     if (scrollHeight < ta.scrollHeight) {
+  //       /// now try and scan the ta's height downwards
+  //       /// until scrollHeight becomes larger than height
+  //       while (ta.offsetHeight >= ta.scrollHeight) {
+  //         ta.style.height = (height -= scanAmount) + "px";
+  //       }
+  //       /// be more specific to get the exact height
+  //       while (ta.offsetHeight < ta.scrollHeight) {
+  //         ta.style.height = height++ + "px";
+  //       }
+  //       /// reset the ta back to it's original height
+  //       ta.style.height = origHeight;
+  //       /// put the overflow back
+  //       ta.style.overflow = overflow;
+  //       return height;
+  //     }
+  //   } else {
+  //     return scrollHeight;
+  //   }
+  // };
+
+  // var calculateHeight = function () {
+  //   var ta = document.getElementById("ta"),
+  //     style = window.getComputedStyle
+  //       ? window.getComputedStyle(ta)
+  //       : ta.currentStyle,
+  //     // This will get the line-height only if it is set in the css,
+  //     // otherwise it's "normal"
+  //     taLineHeight = parseInt(style.lineHeight, 10),
+  //     // Get the scroll height of the textarea
+  //     taHeight = calculateContentHeight(ta, taLineHeight),
+  //     // calculate the number of lines
+  //     numberOfLines = Math.ceil(taHeight / taLineHeight);
+
+  //   document.getElementById("lines").innerHTML =
+  //     "there are " + numberOfLines + " lines in the text area";
+  // };
+
+  // calculateHeight();
+  // if (ta.addEventListener) {
+  //   ta.addEventListener("mouseup", calculateHeight, false);
+  //   ta.addEventListener("keyup", calculateHeight, false);
+  // } else if (ta.attachEvent) {
+  //   // IE
+  //   ta.attachEvent("onmouseup", calculateHeight);
+  //   ta.attachEvent("onkeyup", calculateHeight);
+  // }
   return (
     <form className="main_tweet_textform" id={props.id} onSubmit={props.add}>
       <textarea
@@ -8,6 +68,8 @@ const MainTweetTextArea = (props) => {
         onChange={props.handle}
         value={props.value}
         cols="33"
+        wrap="hard"
+        id="ta"
       ></textarea>
     </form>
   );
